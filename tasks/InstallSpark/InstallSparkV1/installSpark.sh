49aba3f9a6afe15c5e61d7762e6f32d781d034be
Ricardo Serradas <riserrad@microsoft.com>
2019-07-10 18:13:24 -0700

    Adding Install Spark task

---
+++ tasks/InstallSpark/InstallSparkV1/installSpark.sh
@@ -0,0 +1,28 @@
+# ===================================================================================
+#
+# FILE:  2-install-spark.sh
+#
+# USAGE:  bash 2-install-spark.sh
+#
+#   DESCRIPTION: Installs Spark on the Build server
+#                Copies the test data file to the "docs" folder
+#                Useful for unit testing
+#
+# NOTES:  ---
+# AUTHOR:  Bruno Terkaly
+# VERSION:  1.0
+# CREATED:  June 10, 2019
+#===================================================================================
+setupSpark() {
+
+   SPARKURL=http://apache.spinellicreations.com/spark/spark-2.4.3/spark-2.4.3-bin-hadoop2.7.tgz
+   sudo wget "$SPARKURL"
+   tar xzvf spark-2.4.3-bin-hadoop2.7.tgz
+   PATH=$PATH:/home/vsts/spark-2.4.3-bin-hadoop2.7/bin
+   echo "Path = "$PATH
+   echo "mkdir -p /dbfs/docs"
+   sudo mkdir -p /dbfs/docs
+   echo "cp MN212142_9392.csv /dbfs/docs"
+   sudo cp "MN212142_9392.csv" "/dbfs/docs"
+}
+setupSpark
