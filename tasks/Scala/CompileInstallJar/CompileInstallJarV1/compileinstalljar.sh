f19b6999f9d58ef5f453f9c86c3b10f0087593ed
Ricardo Serradas <riserrad@microsoft.com>
2019-07-10 21:24:06 -0700

    Added compile install jar

---
+++ tasks/Scala/CompileInstallJar/CompileInstallJarV1/compileinstalljar.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# ===================================================================================
+#
+# FILE:  5-compile-install-jar.sh
+#
+# USAGE:  bash 5-compile-install-jar.sh
+#
+#   DESCRIPTION: (1) Compiles Scala Code and packages as JAR.
+#                (2) Copies production data to cluster for analysis.
+#                (3) Copies and installs JAR file as Spark Library.
+#                Uses API api/2.0/libraries/install
+#                Results in a Run Id that is needed later to validate SUCCESS
+#
+# NOTES:  ---
+# AUTHOR:  Bruno Terkaly
+# VERSION:  1.0
+# CREATED:  June 10, 2019
+#===================================================================================
+#========================
+# Compile and Package
+#========================
+clusterid=$1
+echo "sbt compile package"
+sbt compile package
+#========================
+# Copy data file to cluster
+#========================
+dbfs rm  dbfs:/docs/MN212142_9392.csv
+dbfs cp MN212142_9392.csv dbfs:/docs
+#========================
+# Install new jar
+#========================
+echo "Install new jar"
+dbfs rm dbfs:/jar/sparkcode.jar
+SOURCEJAR=./target/scala-2.11/sparkcode_2.11-1.0.jar
+dbfs cp $SOURCEJAR dbfs:/jar/sparkcode.jar
+dbfs ls dbfs:/jar/sparkcode.jar
+curl -n -H "Content-Type: application/json" -X POST -d @- https://centralus.azuredatabricks.net/api/2.0/libraries/install <<JSON
+{
+  "cluster_id": "$clusterid",
+  "libraries": [
+    {
+      "jar": "dbfs:/jar/sparkcode.jar"
+    }
+  ]
+}
+JSON
---
+++ tasks/Scala/CompileInstallJar/CompileInstallJarV1/compileinstalljar.ts
@@ -0,0 +1,67 @@
+import path = require('path')
+import tl = require('azure-pipelines-task-lib');
+import tr = require('azure-pipelines-task-lib/toolrunner')
+
+async function run() {
+    try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
+
+        const workingDirectory: string = tl.getInput('workingDirectory', false);
+
+        if(workingDirectory != ''){
+            tl.cd(workingDirectory);
+        }
+        
+        const clusterid: string = tl.getInput('clusterid', true);
+        
+        let bashPath: string = tl.which('bash', true);
+        let fileName = 'startCluster.sh'
+        let filePath = path.join(__dirname, fileName);
+
+        let bash = tl.tool(bashPath);
+
+        bash.arg([
+            filePath,
+            clusterid
+        ]);
+
+        let options = <tr.IExecOptions>{
+            cwd: __dirname,
+            env: {},
+            silent: false,
+            failOnStdErr: false,
+            errStream: process.stdout,
+            outStream: process.stdout,
+            ignoreReturnCode: true,
+            windowsVerbatimArguments: false
+        };
+
+        // Listen for stderr.
+        let stderrFailure = false;
+        bash.on('stderr', (data) => {
+            stderrFailure = true;
+        });
+
+        let exitCode: number = await bash.exec(options);
+
+        let result = tl.TaskResult.Succeeded;
+
+        if (exitCode !== 0) {
+            tl.error(tl.loc('JS_ExitCode', exitCode));
+            result = tl.TaskResult.Failed
+        }
+
+        // Fail on stderr.
+        if (stderrFailure) {
+            tl.error(tl.loc('JS_Stderr'));
+            result = tl.TaskResult.Failed;
+        }
+
+        tl.setResult(result, "", true);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
