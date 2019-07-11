892850e1301f940441ef72822e4fb96dfdc7da67
Ricardo Serradas <riserrad@microsoft.com>
2019-07-10 17:57:45 -0700

    Added task for start cluster

---
+++ tasks/StartCluster/StartClusterV1/startCluster.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+# ===================================================================================
+#
+# FILE:  0-start-cluster.sh
+#
+# USAGE:  bash 0-start-cluster.sh ...
+#
+#   DESCRIPTION: Uses Databricks API to put Spark cluster into a running state.
+#                Uses databricks utility to check for "RUNNING" state.
+#                Uses api/2.0/clusters/start to start cluster.
+#
+# NOTES:  ---
+# AUTHOR:  Bruno Terkaly
+# VERSION:  1.0
+# CREATED:  June 10, 2019
+#===================================================================================
+
+#=======================================================
+# Purpose: Start cluster
+#  - Asssumptions: Just one cluster to check
+#                  Add code for more than one cluster
+#=======================================================
+lookfor=RUNNING
+clusterStatus=$(databricks clusters list | awk '{print $3}')
+if [ "$clusterStatus" != "$lookfor" ]
+then
+   echo "cluster not running, so turn on"
+else
+   echo "cluster already running, so exit"
+   exit 0
+fi
+
+#=======================================================
+# Since cluster is not running, run it
+#=======================================================
+clusterid=$(databricks clusters list | awk '{print $1}')
+curl -n -H "Content-Type: application/json" -X POST -d @- https://centralus.azuredatabricks.net/api/2.0/clusters/start <<JSON
+{
+  "cluster_id": "$clusterid"
+}
+JSON
+#=======================================================
+# Now loop (stay here) until running
+#=======================================================
+clusterStatus=$(databricks clusters list | awk '{print $3}')
+echo $clusterStatus
+while [ "$clusterStatus" != "$lookfor" ]
+do
+    sleep 1
+    echo "Restarting..."
+    clusterStatus=$(databricks clusters list | awk '{print $3}')
+done
+echo "Running now..."
---
+++ tasks/StartCluster/StartClusterV1/startCluster.ts
@@ -0,0 +1,60 @@
+import path = require('path')
+import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner')
+
+async function run() {
+    try {
+        tl.setResourcePath(path.join(__dirname, 'task.json'));
+        
+        const clusterid: string = tl.getInput('clusterid', true);
+        
+        let bashPath: string = tl.which('bash', true);
+        let fileName = 'startCluster.sh'
+
+        let bash = tl.tool(bashPath);
+
+        bash.arg([
+            fileName,
+            clusterid
+        ]);
+
+        let options = <tr.IExecOptions>{
+            cwd: __dirname,
+            env: {},
+            silent: false,
+            failOnStdErr: false,
+            errStream: process.stdout,
+            outStream: process.stdout,
+            ignoreReturnCode: true,
+            windowsVerbatimArguments: false
+        };
+
+        // Listen for stderr.
+        let stderrFailure = false;
+        bash.on('stderr', (data) => {
+            stderrFailure = true;
+        });
+
+        let exitCode: number = await bash.exec(options);
+
+        let result = tl.TaskResult.Succeeded;
+
+        if (exitCode !== 0) {
+            tl.error(tl.loc('JS_ExitCode', exitCode));
+            result = tl.TaskResult.Failed
+        }
+
+        // Fail on stderr.
+        if (stderrFailure) {
+            tl.error(tl.loc('JS_Stderr'));
+            result = tl.TaskResult.Failed;
+        }
+
+        tl.setResult(result, "", true);
+    }
+    catch (err) {
+        tl.setResult(tl.TaskResult.Failed, err.message);
+    }
+}
+
+run();
