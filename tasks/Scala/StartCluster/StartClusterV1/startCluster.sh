3768720d7ee766e68fbebb2aba45ffe7f6ea2e57
Ricardo Serradas <riserrad@microsoft.com>
2019-07-12 10:32:20 -0700

    Adjusting startCluster.sh

--- tasks/Scala/StartCluster/StartClusterV1/startCluster.sh
+++ tasks/Scala/StartCluster/StartClusterV1/startCluster.sh
@@ -23,16 +23,10 @@
 clusterid=$1
 lookfor=RUNNING
 #clusterStatus=$(databricks clusters list | awk '{print $3}')
-clusterStatus=$(databricks clusters get --cluster-id $clusterid | awk '{print $3}')
+clusterStatus=$(databricks clusters get --cluster-id $clusterid --profile AZDO | jq -r .state)
 if [ "$clusterStatus" != "$lookfor" ]
 then
    echo "cluster not running, so turn on"
-
-   curl -n -H "Content-Type: application/json" -X POST -d @- https://centralus.azuredatabricks.net/api/2.0/clusters/start <<JSON
-   {
-   "cluster_id": "$clusterid"
-   }
-   JSON
 else
    echo "cluster already running, so exit"
    exit 0
@@ -41,21 +35,16 @@ fi
 #=======================================================
 # Since cluster is not running, run it
 #=======================================================
-clusterid=$(databricks clusters list | awk '{print $1}')
-curl -n -H "Content-Type: application/json" -X POST -d @- https://centralus.azuredatabricks.net/api/2.0/clusters/start <<JSON
-{
-  "cluster_id": "$clusterid"
-}
-JSON
+databricks clusters start --cluster-id $clusterid --profile AZDO
 #=======================================================
 # Now loop (stay here) until running
 #=======================================================
-clusterStatus=$(databricks clusters list | awk '{print $3}')
+clusterStatus=$(databricks clusters get --cluster-id $clusterid --profile AZDO | jq -r .state)
 echo $clusterStatus
 while [ "$clusterStatus" != "$lookfor" ]
 do
     sleep 1
     echo "Restarting..."
-    clusterStatus=$(databricks clusters list | awk '{print $3}')
+    clusterStatus=$(databricks clusters get --cluster-id $clusterid --profile AZDO | jq -r .state)
 done
 echo "Running now..."
