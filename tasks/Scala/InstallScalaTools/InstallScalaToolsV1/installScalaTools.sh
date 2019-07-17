0892e9ad23a91843409bf24be7ac15f29ee363ed
Ricardo Serradas <riserrad@microsoft.com>
2019-07-16 22:39:11 -0700

    Changes approach to run non-interactive

--- tasks/Scala/InstallScalaTools/InstallScalaToolsV1/installScalaTools.sh
+++ tasks/Scala/InstallScalaTools/InstallScalaToolsV1/installScalaTools.sh
@@ -15,66 +15,63 @@
 setupJava() {
 
     # - Java
-    echo "sudo apt-get update"
-    sudo apt-get update -y
-    echo "sudo apt-get install default-jdk"
-    sudo apt-get install default-jdk -y
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install default-jdk"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install default-jdk -y
 }
 setupScalaAndSbt() {
 
     # - Scala
-    echo "sudo apt-get remove scala-library scala"
-    sudo apt-get remove scala-library scala -y
-    echo "sudo wget http://scala-lang.org/files/archive/scala-2.12.1.deb"
-    sudo wget http://scala-lang.org/files/archive/scala-2.12.1.deb
-    echo "sudo dpkg -i scala-2.12.1.deb"
-    sudo dpkg -i scala-2.12.1.deb
-    echo "sudo apt-get update"
-    sudo apt-get update -y
-    echo "sudo apt-get install scala"
-    sudo apt-get install scala -y
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get remove scala-library scala"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get remove scala-library scala -y
+    echo "sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.1.deb"
+    sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.1.deb
+    echo "sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.1.deb"
+    sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.1.deb
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install scala"
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install scala -y
     echo "==================="
-    echo "sudo apt-get update"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
     echo "==================="
-    sudo apt-get update -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
     echo "==================="
-    echo "sudo apt-get install -y apt-transport-https"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https"
     echo "==================="
-    sudo apt-get install -y apt-transport-https -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https -y
 
     echo "==================="
     echo "echo deb..."
     echo "==================="
-    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo DEBIAN_FRONTEND=noninteractive tee -a /etc/apt/sources.list.d/sbt.list
 
     echo "==================="
-    echo "echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list"
+    echo "echo "deb https://dl.bintray.com/sbt/debian /" | sudo DEBIAN_FRONTEND=noninteractive tee -a /etc/apt/sources.list.d/sbt.list"
     echo "==================="
-    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo DEBIAN_FRONTEND=noninteractive tee -a /etc/apt/sources.list.d/sbt.list
 
     echo "==================="
-    echo "sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823"
     echo "==================="
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 
     echo "==================="
-    echo "sudo apt-get update"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
     echo "==================="
-    sudo apt-get update -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
     echo "==================="
-    echo "sudo apt-get install -y sbt"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sbt"
     echo "==================="
-    sudo apt-get install -y sbt -y
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sbt -y
 
 }
 #================================================
 # Main
 #================================================
-
-# As it runs on a non interactive session, it must be explicitly specified.
-echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
 
 setupJava
 setupScalaAndSbt
