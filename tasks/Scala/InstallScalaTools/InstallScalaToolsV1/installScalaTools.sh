1996e5edb7d9c925d15cdce60d30d198d3f168a3
Ricardo Serradas <riserrad@microsoft.com>
2019-07-16 23:52:40 -0700

    Major update on installscalasbt

--- tasks/Scala/InstallScalaTools/InstallScalaToolsV1/installScalaTools.sh
+++ tasks/Scala/InstallScalaTools/InstallScalaToolsV1/installScalaTools.sh
@@ -12,18 +12,23 @@
 # VERSION:  1.0
 # CREATED:  June 10, 2019
 #===================================================================================
-prepareAptGet() {
+prepareToInstall() {
+    echo "==================="
+    echo "sudo apt-get remove unscd"
+    echo "==================="
+
+    sudo apt-get remove unscd -y
+    
     echo "==================="
     echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
     echo "==================="
 
     sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
-
+    
     echo "==================="
-    echo "sudo apt-get remove unscd"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install apt-transport-https -y"
     echo "==================="
-
-    sudo apt-get remove unscd
+    sudo DEBIAN_FRONTEND=noninteractive apt-get software-properties-common install apt-transport-https -y
 }
 
 setupJava() {
@@ -35,57 +40,65 @@ setupJava() {
 
     sudo DEBIAN_FRONTEND=noninteractive apt-get install default-jdk -y
 }
-setupScalaAndSbt() {
 
-    # - Scala
+installDebianInstaller() {
     echo "==================="
-    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get remove scala-library scala"
+    echo "sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.6.deb"
     echo "==================="
 
-    sudo DEBIAN_FRONTEND=noninteractive apt-get remove scala-library scala -y
+    sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.6.deb
     
     echo "==================="
-    echo "sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.1.deb"
+    echo "sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.6.deb"
     echo "==================="
 
-    sudo DEBIAN_FRONTEND=noninteractive wget http://scala-lang.org/files/archive/scala-2.12.1.deb
-    
+    sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.6.deb
+}
+
+installScala() {
     echo "==================="
-    echo "sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.1.deb"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
     echo "==================="
 
-    sudo DEBIAN_FRONTEND=noninteractive dpkg -i scala-2.12.1.deb
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
 
     echo "==================="
     echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install scala"
     echo "==================="
 
     sudo DEBIAN_FRONTEND=noninteractive apt-get install scala -y
-    
-    echo "==================="
-    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https"
-    echo "==================="
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y apt-transport-https -y
+}
 
+installSbt() {
     echo "==================="
     echo "echo deb..."
     echo "==================="
+    
     echo "deb https://dl.bintray.com/sbt/debian /" | sudo DEBIAN_FRONTEND=noninteractive tee -a /etc/apt/sources.list.d/sbt.list
 
     echo "==================="
     echo "sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823"
     echo "==================="
+    
     sudo DEBIAN_FRONTEND=noninteractive apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
 
     echo "==================="
-    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sbt"
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get update"
     echo "==================="
-    sudo DEBIAN_FRONTEND=noninteractive apt-get install -y sbt -y
 
+    sudo DEBIAN_FRONTEND=noninteractive apt-get update -y
+
+    echo "==================="
+    echo "sudo DEBIAN_FRONTEND=noninteractive apt-get install sbt -y"
+    echo "==================="
+
+    sudo DEBIAN_FRONTEND=noninteractive apt-get install sbt -y
 }
 #================================================
 # Main
 #================================================
-prepareAptGet
+prepareToInstall
 setupJava
-setupScalaAndSbt
+installDebianInstaller
+installScala
+installSbt
