e5772d90c16fae96cb8d7afc16a0c3fdb7b479d9
Ricardo Serradas <riserrad@microsoft.com>
2019-07-17 18:11:28 -0700

    Adjusted startcluster task

--- tasks/Scala/StartCluster/StartClusterV1/startCluster.js
+++ tasks/Scala/StartCluster/StartClusterV1/startCluster.js
@@ -8,27 +8,79 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const fs = require("fs");
 const path = require("path");
-const tl = require("azure-pipelines-task-lib");
+const tl = require("azure-pipelines-task-lib/task");
+var uuidV4 = require('uuid/v4');
+const noProfile = tl.getBoolInput('noProfile');
+const noRc = tl.getBoolInput('noRc');
+function translateDirectoryPath(bashPath, directoryPath) {
+    return __awaiter(this, void 0, void 0, function* () {
+        let bashPwd = tl.tool(bashPath)
+            .arg('--noprofile')
+            .arg('--norc')
+            .arg('-c')
+            .arg('pwd');
+        let bashPwdOptions = {
+            cwd: directoryPath,
+            env: {},
+            silent: false,
+            failOnStdErr: true,
+            errStream: process.stdout,
+            outStream: process.stdout,
+            ignoreReturnCode: false,
+            windowsVerbatimArguments: false
+        };
+        let pwdOutput = '';
+        bashPwd.on('stdout', (data) => {
+            pwdOutput += data.toString();
+        });
+        yield bashPwd.exec(bashPwdOptions);
+        pwdOutput = pwdOutput.trim();
+        if (!pwdOutput) {
+            throw new Error(`Unable to translate the path '${directoryPath}' to the Linux file system.`);
+        }
+        return `${pwdOutput}`;
+    });
+}
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             tl.setResourcePath(path.join(__dirname, 'task.json'));
-            const workingDirectory = tl.getInput('workingDirectory', false);
-            if (workingDirectory != '') {
-                tl.cd(workingDirectory);
-            }
-            const clusterid = tl.getInput('clusterid', true);
+            // Get inputs.
+            let input_failOnStderr = tl.getBoolInput('failOnStderr', false);
+            let input_clusterid = tl.getInput('clusterid', true);
+            let fileNameOnExtension = 'startCluster.sh';
+            let filePathOnExtension = path.join(__dirname, fileNameOnExtension);
+            console.log("Generating script.");
             let bashPath = tl.which('bash', true);
-            let fileName = 'startCluster.sh';
-            let filePath = path.join(__dirname, fileName);
+            let contents;
+            contents = `. '${filePathOnExtension.replace("'", "'\\''")} ${input_clusterid}'`.trim();
+            console.log(`Formatted command: ${contents}`);
+            // Write the script to disk.
+            tl.assertAgent('2.115.0');
+            // let tempDirectory = tl.getVariable('agent.tempDirectory');
+            let tempDirectory = "./temp";
+            // tl.checkPath(tempDirectory, `${tempDirectory} (agent.tempDirectory)`);
+            let fileName = uuidV4() + '.sh';
+            let filePath = path.join(tempDirectory, fileName);
+            yield fs.writeFileSync(filePath, contents, { encoding: 'utf8' });
+            // Translate the script file path from Windows to the Linux file system.
+            if (process.platform == 'win32') {
+                filePath = (yield translateDirectoryPath(bashPath, tempDirectory)) + '/' + fileName;
+            }
+            // Create the tool runner.
+            console.log('========================== Starting Command Output ===========================');
             let bash = tl.tool(bashPath);
-            bash.arg([
-                filePath,
-                clusterid
-            ]);
+            if (noProfile) {
+                bash.arg('--noprofile');
+            }
+            if (noRc) {
+                bash.arg('--norc');
+            }
+            bash.arg(filePath);
             let options = {
-                cwd: __dirname,
+                cwd: tempDirectory,
                 env: {},
                 silent: false,
                 failOnStdErr: false,
@@ -39,24 +91,28 @@ function run() {
             };
             // Listen for stderr.
             let stderrFailure = false;
-            bash.on('stderr', (data) => {
-                stderrFailure = true;
-            });
+            if (input_failOnStderr) {
+                bash.on('stderr', (data) => {
+                    stderrFailure = true;
+                });
+            }
+            // Run bash.
             let exitCode = yield bash.exec(options);
             let result = tl.TaskResult.Succeeded;
+            // Fail on exit code.
             if (exitCode !== 0) {
-                tl.error(tl.loc('JS_ExitCode', exitCode));
+                tl.error(`Bash exited with code ${exitCode}`);
                 result = tl.TaskResult.Failed;
             }
             // Fail on stderr.
             if (stderrFailure) {
-                tl.error(tl.loc('JS_Stderr'));
+                tl.error("Bash wrote one or more lines to the standard error stream.");
                 result = tl.TaskResult.Failed;
             }
             tl.setResult(result, "", true);
         }
         catch (err) {
-            tl.setResult(tl.TaskResult.Failed, err.message);
+            tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed', true);
         }
     });
 }
--- tasks/Scala/StartCluster/StartClusterV1/startCluster.ts
+++ tasks/Scala/StartCluster/StartClusterV1/startCluster.ts
@@ -1,54 +1,118 @@
-import path = require('path')
-import tl = require('azure-pipelines-task-lib');
-import tr = require('azure-pipelines-task-lib/toolrunner')
+import fs = require('fs');
+import path = require('path');
+import os = require('os');
+import tl = require('azure-pipelines-task-lib/task');
+import tr = require('azure-pipelines-task-lib/toolrunner');
+var uuidV4 = require('uuid/v4');
+
+const noProfile = tl.getBoolInput('noProfile');
+const noRc = tl.getBoolInput('noRc');
+
+async function translateDirectoryPath(bashPath: string, directoryPath: string): Promise<string> {
+    let bashPwd = tl.tool(bashPath)
+        .arg('--noprofile')
+        .arg('--norc')
+        .arg('-c')
+        .arg('pwd');
+
+    let bashPwdOptions = <tr.IExecOptions>{
+        cwd: directoryPath,
+        env: {},
+        silent: false,
+        failOnStdErr: true,
+        errStream: process.stdout,
+        outStream: process.stdout,
+        ignoreReturnCode: false,
+        windowsVerbatimArguments: false
+    };
+    let pwdOutput = '';
+    bashPwd.on('stdout', (data) => {
+        pwdOutput += data.toString();
+    });
+    await bashPwd.exec(bashPwdOptions);
+    pwdOutput = pwdOutput.trim();
+    if (!pwdOutput) {
+        throw new Error(`Unable to translate the path '${directoryPath}' to the Linux file system.`);
+    }
+
+    return `${pwdOutput}`;
+}
 
 async function run() {
     try {
         tl.setResourcePath(path.join(__dirname, 'task.json'));
 
-        const workingDirectory: string = tl.getInput('workingDirectory', false);
+        // Get inputs.
+        let input_failOnStderr = tl.getBoolInput('failOnStderr', false);
+        let input_clusterid: string = tl.getInput('clusterid', true);
 
-        if(workingDirectory != ''){
-            tl.cd(workingDirectory);
-        }
-        
-        const clusterid: string = tl.getInput('clusterid', true);
-        
+        let fileNameOnExtension = 'startCluster.sh';
+        let filePathOnExtension = path.join(__dirname, fileNameOnExtension);
+
+        console.log("Generating script.");
         let bashPath: string = tl.which('bash', true);
-        let fileName = 'startCluster.sh'
-        let filePath = path.join(__dirname, fileName);
+        let contents: string;
 
-        let bash = tl.tool(bashPath);
+        contents = `. '${filePathOnExtension.replace("'", "'\\''")} ${input_clusterid}'`.trim();
+        console.log(`Formatted command: ${contents}`);
 
-        bash.arg([
+        // Write the script to disk.
+        tl.assertAgent('2.115.0');
+        // let tempDirectory = tl.getVariable('agent.tempDirectory');
+        let tempDirectory = "./temp"
+        // tl.checkPath(tempDirectory, `${tempDirectory} (agent.tempDirectory)`);
+        
+        let fileName = uuidV4() + '.sh';
+        let filePath = path.join(tempDirectory, fileName);
+        await fs.writeFileSync(
             filePath,
-            clusterid
-        ]);
+            contents,
+            { encoding: 'utf8' });
+
+        // Translate the script file path from Windows to the Linux file system.
+        if (process.platform == 'win32') {
+            filePath = await translateDirectoryPath(bashPath, tempDirectory) + '/' + fileName;
+        }
+
+        // Create the tool runner.
+        console.log('========================== Starting Command Output ===========================');
+        let bash = tl.tool(bashPath);
+        if (noProfile) {
+            bash.arg('--noprofile');
+        }
+        if (noRc) {
+            bash.arg('--norc');
+        }
+        bash.arg(filePath);
 
         let options = <tr.IExecOptions>{
-            cwd: __dirname,
+            cwd: tempDirectory,
             env: {},
             silent: false,
             failOnStdErr: false,
-            errStream: process.stdout,
-            outStream: process.stdout,
+            errStream: process.stdout, // Direct all output to STDOUT, otherwise the output may appear out
+            outStream: process.stdout, // of order since Node buffers it's own STDOUT but not STDERR.
             ignoreReturnCode: true,
             windowsVerbatimArguments: false
         };
 
         // Listen for stderr.
         let stderrFailure = false;
-        bash.on('stderr', (data) => {
-            stderrFailure = true;
-        });
+        if (input_failOnStderr) {
+            bash.on('stderr', (data) => {
+                stderrFailure = true;
+            });
+        }
 
+        // Run bash.
         let exitCode: number = await bash.exec(options);
 
         let result = tl.TaskResult.Succeeded;
 
+        // Fail on exit code.
         if (exitCode !== 0) {
-            tl.error("Bash exited with code " + exitCode);
-            result = tl.TaskResult.Failed
+            tl.error(`Bash exited with code ${exitCode}`);
+            result = tl.TaskResult.Failed;
         }
 
         // Fail on stderr.
@@ -60,7 +124,7 @@ async function run() {
         tl.setResult(result, "", true);
     }
     catch (err) {
-        tl.setResult(tl.TaskResult.Failed, err.message);
+        tl.setResult(tl.TaskResult.Failed, err.message || 'run() failed', true);
     }
 }
 
