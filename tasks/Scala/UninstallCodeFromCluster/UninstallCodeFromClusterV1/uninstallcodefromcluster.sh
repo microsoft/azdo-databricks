86e06ef4201c37c1dc76d1750d7b2814903cccfd
Ricardo Serradas <riserrad@microsoft.com>
2019-07-21 14:12:03 -0700

    Refactoring Uninstall Library

--- tasks/Scala/UninstallCodeFromCluster/UninstallCodeFromClusterV1/uninstallcodefromcluster.sh
+++ tasks/Scala/UninstallCodeFromCluster/UninstallCodeFromClusterV1/uninstallcodefromcluster.sh
@@ -14,13 +14,42 @@
 # VERSION:  1.0
 # CREATED:  June 10, 2019
 #===================================================================================
-#========================
-# Un-install jar
-#========================
 clusterid=$1
-libraryFileName=$2
-databricks libraries uninstall --cluster-id $clusterid --jar "dbfs:/jar/$libraryFileName" --profile AZDO
-#========================
-# Restart cluster
-#========================
-databricks clusters restart --cluster-id $clusterid --profile AZDO
+packagename=$2
+
+uninstallPackage() {
+    clusterid=$1
+    packagename=$2
+    databricks libraries uninstall --cluster-id $clusterid --jar "dbfs:/jar/$packagename.jar" --profile AZDO
+}
+
+restartCluster() {
+    clusterid=$1
+    databricks clusters restart --cluster-id $clusterid --profile AZDO
+}
+
+waitClusterReboot() {
+    clusterid=$1
+
+    lookfor=RUNNING
+
+    clusterStatus=$(databricks clusters get --cluster-id $clusterid --profile AZDO | jq -r .state)
+
+    if [ "$clusterStatus" == "TERMINATED"]
+    do
+        echo "The cluster $clusterid is not rebooting."
+        exit 1
+    done
+
+    while [ "$clusterStatus" != "$lookfor" ]
+    do
+        sleep 10
+        echo "Restarting $clusterid..."
+        clusterStatus=$(databricks clusters get --cluster-id $clusterid --profile AZDO | jq -r .state)
+    done
+    echo "Running now..."
+}
+
+uninstallPackage $clusterid $packagename
+restartCluster $clusterid
+waitClusterReboot $clusterid
